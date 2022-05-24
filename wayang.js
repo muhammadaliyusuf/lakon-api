7ded64506df3ed94c0a8e2e1747e8514fd535d3e
Muhammad Ali Yusuf <48145052+muhammadaliyusuf@users.noreply.github.com>
2022-05-25 06:48:47 +0700

    Perbaikan Error

--- wayang.js
+++ wayang.js
@@ -1,5 +1,5 @@
-# array untuk menampung objek wayang
+// array untuk menampung objek wayang
 const wayang = [];
 
-# module agar dapet digunakan file lain
+// module agar dapet digunakan file lain
 module.exports = wayang;
